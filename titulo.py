0f2f4467fd2041fc00929682638134ffbe56f146
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2024-07-28 11:15:23 -0300

    titulo

--- titulo.py
+++ titulo.py
@@ -1,5 +1,7 @@
 import pygame
 
+fonte = pygame.font.SysFont('arial', 40, True, True)
+fonte1 = pygame.font.SysFont('arial', 25, True, True)
 class Titulo:
 	def __init__(self):
 		self.bg = pygame.image.load("titulo/titulo.jpeg")
@@ -11,5 +13,11 @@ class Titulo:
 
 	def frame(self, screen, delta, jogo):
 		screen.blit(pygame.transform.scale(self.bg, screen.get_size()), (0, 0))
+		mensagem = 'Pressione qualquer tecla para iniciar'
+		mensagem1 = 'Ou pressione esc para sair'
+		texto_formatado = fonte.render(mensagem, True, (000,000,000))
+		texto_formatado1 = fonte1.render(mensagem1, True, (000,000,000))
+		screen.blit(texto_formatado, (200, 480))
+		screen.blit(texto_formatado1, (370, 530))
 		if self.comecar:
 			return "novo jogo"
