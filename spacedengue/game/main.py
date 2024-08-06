ca3677661e5388fd8877a026b6ff05640f280175
Moisés Iatagan <freakyatagan@gmail.com>
2024-08-06 14:24:35 -0300

    start do space dengue

---
+++ spacedengue/game/main.py
@@ -0,0 +1,31 @@
+import pygame, sys
+from player import Player
+
+class Game:
+    def __init__(self):
+        player_sprit = Player((300,300))
+        self.player = pygame.sprite.GroupSingle()
+
+    def run(self):
+        self.player.draw(screen)
+        # update all sprite groups
+        # draw all sprite groups
+
+if __name__ == "__main__":
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    screen.fill((30,30,30))
+    game.run()
+
+    pygame.display.flip()
+    clock.tick(60)
---
+++ spacedengue/game/player.py
@@ -0,0 +1,7 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.imag = pygame.image.load("spacedengue/game/graphics").convert_alpha()
+        self.rect = self.image.get_rect(midbottom = pos)
