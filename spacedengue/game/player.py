96784f1274e4087f4e2b0fd7163b89db7ceb133c
Moisés Iatagan <freakyatagan@gmail.com>
2024-08-06 18:50:16 -0300

    space

--- spacedengue/game/player.py
+++ spacedengue/game/player.py
@@ -3,5 +3,5 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos):
         super().__init__()
-        self.imag = pygame.image.load("spacedengue/game/graphics").convert_alpha()
+        self.imag = pygame.image.load("spacedengue/game/graphics/player.png").convert_alpha()
         self.rect = self.image.get_rect(midbottom = pos)
