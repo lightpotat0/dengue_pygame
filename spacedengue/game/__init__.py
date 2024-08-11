93a91bd0ef63b81139f1501253fa3c00988f10e3
Moisés Iatagan <freakyatagan@gmail.com>
2024-08-11 14:29:41 -0300

    update sapce dengue sla

--- spacedengue/game/__init__.py
+++ spacedengue/game/__init__.py
@@ -12,16 +12,23 @@ class Game:
         self.shape = obstacle.shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
-        self.create_obstacle(40,480)
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_obstacle_obstacles(*self.obstacle_x_positions,x_start = screen_width / 15,y_start = 480)
         
-    def create_obstacle(self, x_start, y_start):
+    def create_obstacle(self, x_start, y_start,offset_x):
         for row_index, row in enumerate(self.shape):
             for col_index,col in enumerate(row):
                 if col == 'x':
-                    x = x_start + col_index * self.block_size
+                    x = x_start + col_index * self.block_size + offset_x
                     y = y_start + row_index * self.block_size
                     block = obstacle.Block(self.block_size,(241,79,80),x,y)
                     self.blocks.add(block)
+    
+    def create_multiple_obstacles(self,x_start, y_start,*offset):
+        for x in offset:
+            self.create_obstacle(x_start,y_start,offset_x)
+
     def run(self):
         self.player.update()
 
