241d97a6df7c495863ebea359a95de93e9ee7451
Moisés Iatagan <freakyatagan@gmail.com>
2024-08-11 01:19:04 -0300

    toma space commit

--- spacedengue/game/__init__.py
+++ spacedengue/game/__init__.py
@@ -1,16 +1,34 @@
 import pygame, sys
 from player import Player
-
+import obstacle
 class Game:
     def __init__(self):
+        # Player Setup
         player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
+
+        # Obstacle Setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.create_obstacle(40,480)
+        
+    def create_obstacle(self, x_start, y_start):
+        for row_index, row in enumerate(self.shape):
+            for col_index,col in enumerate(row):
+                if col == 'x':
+                    x = x_start + col_index * self.block_size
+                    y = y_start + row_index * self.block_size
+                    block = obstacle.Block(self.block_size,(241,79,80),x,y)
+                    self.blocks.add(block)
     def run(self):
         self.player.update()
 
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
+
+        self.blocks.draw(screen)
         # update all sprite groups
         # draw all sprite groups
 
