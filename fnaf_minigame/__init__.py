9566fe688a02163a6f53cd23302ac009296b97c3
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2024-08-17 13:41:18 -0300

    mosquitodoor

--- fnaf_minigame/__init__.py
+++ fnaf_minigame/__init__.py
@@ -7,15 +7,16 @@ class Cenario:
         self.bg = pygame.image.load('fnaf_minigame/sprites/cenario.png')
         self.mosquito_over = pygame.image.load('fnaf_minigame/sprites/bain.png')
         self.mosquito_posicao = "lá"
-        #self.musquito1 = pygame.image.load('fnaf_minigame/sprites/')
+        self.mosquito_situacion = "vivo"
+        self.musquito_door = pygame.image.load('fnaf_minigame/sprites/mosquito_door1.png')
         #self.musquito2 = pygame.image.load('fnaf_minigame/sprites/')
         #self.click = pygame.image.load('fnaf_minigame/sprites/')
-        #self.mosquito_die1 = pygame.image.load('fnaf_minigame/sprites/')
+        #self.mosquito_die1 = pygame.image.load('fnaf_minigame/sprites/mosquito_die1')
         #self.mosquito_die2 = pygame.image.load('fnaf_minigame/sprites/')
         self.cont = 0
  
     def frame(self, screen, delta, jogo):
-        if self.cont < 2:
+        if self.cont < 0:
             self.cont += delta
         else:
             self.mosquito_posicao = "aqui"
@@ -23,4 +24,6 @@ class Cenario:
         screen.blit(pygame.transform.scale(self.bg, screen.get_size()), (0, 0))
         match self.mosquito_posicao:
             case "aqui":
-                util.scaleblit(screen, 180, self.mosquito_over, (120, 50))
+                util.scaleblit(screen, 180, self.musquito_door, (0, 55))
+
+            
